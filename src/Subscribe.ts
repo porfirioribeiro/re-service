2f4f4e477dfb7faa0a77d87dd4d1fa4c3ddc474a
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2018-04-22 13:28:43 +0100

    Update packages, fixed observedBits to be instance based, for good now!

--- src/Subscribe.ts
+++ src/Subscribe.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { Service, ServiceType } from './Service';
-import { RServiceContext, ContextValue, getBitMaskForServiceTypes } from './utils';
+import { RServiceContext, ContextValue } from './utils';
 
 export interface SubscribeProps {
   to: ServiceType[];
@@ -9,18 +9,24 @@ export interface SubscribeProps {
   children?: (...instances: Service[]) => React.ReactNode;
 }
 
+export interface SubscribeState {
+  observedBits?: number;
+}
+
 /**
  * Subscribe component acts as a connection to the Provider services
  * It will look for services specified on `to` in the Provider and create them if they don't exist
  */
-export class Subscribe extends React.Component<SubscribeProps> {
+export class Subscribe extends React.Component<SubscribeProps, SubscribeState> {
   instances: Service<any>[] = [];
   needsUpdate = true;
+  lastNode: React.ReactNode = null;
+  state: SubscribeState = {};
   /**
    * React 16.3 context has children as a function
    * This function is called with the current
    */
-  renderChild = ({ services, initService, updateService }: ContextValue) => {
+  renderChild = ({ services, initService, updateService, getBitMaskForServices }: ContextValue) => {
     //check if `to` changed and create instances if needed
     if (this.needsUpdate) {
       this.instances = this.props.to.map(serviceType => {
@@ -32,23 +38,37 @@ export class Subscribe extends React.Component<SubscribeProps> {
         instance.onServiceUpdate = updateService;
         return instance;
       });
+      // Calculate the bits we want to observe so this component will only be updated when the subscribed services update
+      this.setState({ observedBits: getBitMaskForServices(this.instances) });
+      /**
+       * We cache last rendered node, at the first render this will be null
+       * We only want to actually render the children after we assign observedBits to the Consumer
+       */
+      return this.lastNode;
     }
 
-    return this.props.render
+    return (this.lastNode = this.props.render
       ? this.props.render(...this.instances)
-      : typeof this.props.children === 'function' ? this.props.children(...this.instances) : this.props.children;
+      : typeof this.props.children === 'function'
+        ? this.props.children(...this.instances)
+        : this.props.children);
   };
 
-  shouldComponentUpdate(nextProps: SubscribeProps) {
-    return (this.needsUpdate =
-      !this.props.to ||
-      this.props.to.length !== nextProps.to.length ||
-      nextProps.to.some((s, i) => s !== this.props.to![i]));
+  /**
+   * Only re-render this component if `to` changed or if we are in setting observedBits phase.
+   */
+  shouldComponentUpdate(nextProps: SubscribeProps, nextState: SubscribeState) {
+    return (
+      (this.needsUpdate =
+        !this.props.to ||
+        this.props.to.length !== nextProps.to.length ||
+        nextProps.to.some((s, i) => s !== this.props.to![i])) || nextState !== this.state
+    );
   }
 
   render() {
     return React.createElement(RServiceContext.Consumer, {
-      unstable_observedBits: getBitMaskForServiceTypes(this.props.to),
+      unstable_observedBits: this.state.observedBits,
       children: this.renderChild
     });
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,39 +1,14 @@
-import { ComponentType, ReactNode, createContext } from 'react';
-import { ServiceMap, Service, ServiceType } from './Service';
-
-export interface Context<T> {
-  Provider: ComponentType<{
-    value: T;
-    children?: ReactNode;
-  }>;
-  Consumer: ComponentType<{
-    children: (value: T) => ReactNode;
-    unstable_observedBits?: number;
-  }>;
-}
-
-const serviceBitMaskMap = new Map<string, number>();
-
-function serviceReducer(mask: number, serviceName: string) {
-  let m = serviceBitMaskMap.get(serviceName);
-  if (!m) serviceBitMaskMap.set(serviceName, (m = 1 << serviceBitMaskMap.size));
-  return (mask |= m);
-}
-
-export function getBitMaskForServices(services: Service[]): number {
-  return services.map(service => service.serviceName).reduce(serviceReducer, 0);
-}
-
-export function getBitMaskForServiceTypes(services: ServiceType[]): number {
-  return services.map(service => service.serviceName).reduce(serviceReducer, 0);
-}
+import { Context, createContext } from 'react';
+import { ServiceMap, Service } from './Service';
 
 const calculateChangedBits = (_prev: ContextValue, next: ContextValue) =>
-  next.changes ? getBitMaskForServices(next.changes) : 0;
+  next.changes ? next.getBitMaskForServices(next.changes) : 0;
 
 export interface ContextValue {
   services: ServiceMap;
+  serviceBitMaskMap: Map<Service, number>;
   changes: Service[] | null;
+  getBitMaskForServices: (services: Service[]) => number;
   initService: <State = {}>(service: Service<State>) => void;
   updateService: <State = {}>(service: Service, prevState: State, changes: Partial<State>) => void;
 }
