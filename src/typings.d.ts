9689519d1b4dc7c2791121f84e71ba2ef246d92d
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2018-11-21 23:42:22 +0000

    Update packages

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,4 @@
 import React, { Context } from 'react';
 declare module 'react' {
-  export function useState<T = any>(value: T): [T, (value: T) => void];
-  export function useContext<T = any>(context: Context<T>, observedBits?: number): T;
+  export function useContext<T>(context: Context<T>, observedBits?: number | boolean): T;
 }
