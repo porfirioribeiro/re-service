5ce345043ee1fd84831a04f51a2460e98c9c430f
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2020-06-06 21:05:06 +0100

    remove postInit and initOptions from Service, make it easier to override constructor  Add options like silent to not notify listeners  And queue to use queueMicrotask on notify listeners to avoid setState in render

--- src/service.ts
+++ src/service.ts
@@ -1,41 +1,47 @@
+/* eslint-disable no-dupe-class-members,no-underscore-dangle */
 import { ServiceListener, Mutable, ServiceCtx } from './types';
 import { batch } from './batch';
 
+export type ServiceInit = { serviceContext: ServiceCtx; serviceName: string };
+
 export abstract class Service<State, InitOptions = {}> {
   state: State;
-  initOptions: InitOptions;
+  readonly initOptions?: InitOptions;
   readonly serviceName: string;
   readonly serviceContext: ServiceCtx;
 
   private _listeners: ServiceListener<any>[] = [];
 
-  protected initState?(initOptions: InitOptions): State;
-  protected postInit?(): void;
+  protected initState?(initOptions?: InitOptions): State;
   protected disposeService?(): void;
 
   static dispose<State>(service: Service<State>) {
     if (service.disposeService) service.disposeService();
   }
 
-  constructor(serviceContext: ServiceCtx, serviceName: string, initOptions: InitOptions) {
-    this.serviceContext = serviceContext;
-    this.serviceName = serviceName;
-    this.initOptions = initOptions;
-
-    this.state = this.initState ? this.initState(initOptions) : ({} as State);
-    if (this.postInit) this.postInit();
+  constructor(init: ServiceInit, options?: InitOptions) {
+    this.serviceContext = init.serviceContext;
+    this.serviceName = init.serviceName;
+    this.initOptions = options;
+    this.state = this.initState ? this.initState(options) : ({} as State);
   }
 
   protected setState(newState: Partial<State>): void;
-  protected setState(newState: State, full: true): void;
-  protected setState(newState: State, full?: boolean) {
+  protected setState(newState: State, o: { full: true; silent?: boolean; queue?: boolean }): void;
+  protected setState(newState: Partial<State>, o: { full?: false; silent?: boolean; queue?: boolean }): void;
+  protected setState(newState: State, { full, silent, queue }: { full?: boolean; silent?: boolean; queue?: boolean } = {}) {
     const state = full ? newState : Object.assign({}, this.state, newState);
     (this as Mutable<Service<State>>).state = state;
-    batch(listeners => {
-      for (let i = 0; i < listeners.length; i += 1) {
-        listeners[i](state);
-      }
-    }, this._listeners);
+    if (!silent) {
+      const callback = () =>
+        batch(listeners => {
+          for (let i = 0; i < listeners.length; i += 1) {
+            listeners[i](state);
+          }
+        }, this._listeners);
+      if (queue) queueMicrotask(callback);
+      else callback();
+    }
   }
 
   subscribe(listener: ServiceListener<State>) {
